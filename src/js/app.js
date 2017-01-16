7d4e68469b6a9f73097189671b262e495ba93a09
Jobarah <barahona.josue@unitec.edu>
2017-01-16 10:37:04 -0600

    redesign

--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,10 @@ import ReactJModal from './ReactJModal';
 import {data} from '../data';
 import {modalStyles} from '../sass/modal_styles';
 
-ReactDOM.render( <ReactJModal modalStyles = {modalStyles}>
+ReactDOM.render(
+	<ReactJModal modalStyles = {modalStyles}>
 		<div>
 			<p>{data}</p>
 		</div>
-	</ReactJModal>, document.getElementById('app'));
+	</ReactJModal>
+, document.getElementById('app'));
